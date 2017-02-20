8ec8dd8484e446319ab0b4026a667f685191a054
Ranu <ranu.miah@outlook.com>
2017-02-20 23:56:57 +0000

    How to add simple Angular 2 component

---
+++ app/events-app.component.ts
@@ -0,0 +1,10 @@
+// '@angular/core' ==> the exact location is in systems.config.js file
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'events-app',
+    template: '<h2>Hello World</h2>'
+})
+
+export class EventsAppCoponent {
+}
