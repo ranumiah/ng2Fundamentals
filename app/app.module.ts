4c81055265f86318eae7ab71e8707bb12b184e82
Ranu <O@O.net>
2017-02-21 09:19:14 +0000

    Add extra notes on Main.ts and App.Module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppCoponent } from './events-app.component'
 
+// So this guy describes the class that follows it
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [EventsAppCoponent],  // Register the Moduel
-    bootstrap: [EventsAppCoponent] // This is the main container module
+    imports: [BrowserModule], // What stuff do I need?
+    declarations: [EventsAppCoponent],  // What's in my app module?
+    bootstrap: [EventsAppCoponent] // Where do I start?
 })
-export class AppModule {}
+export class AppModule { }
+
+
+// All the configuration occurs in the NgModule decorator. The bootstrap property is only necessary in this root NgModule.
+// It instructs Angular to examine the existing DOM (parsed from index.html in this case), identifying the elements to replace with the components defined in bootstrap.
+// The imports property defines any NgModules that are consumed by this NgModule. Being this root NgModule is running in a browser, you must import the BrowserModule.
+// The other NgModules in your application do not need to import that BrowserModule, only the root NgModule.
+// Finally, you declare all of the Components and Directives that belong to this NgModule in declarations.
+// There are several other important properties with the NgModule decorator so refer to the documentation for more information.
--- app/main.ts
+++ app/main.ts
@@ -2,5 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
 import { AppModule } from './app.module'
 
+// This is using (JIT) compilation in the browser with platformBrowserDynamic to bootstrap the AppModule.
 // One time Bootstrap and load the initial main container
 platformBrowserDynamic().bootstrapModule(AppModule)
