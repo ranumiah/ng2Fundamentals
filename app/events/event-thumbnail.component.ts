229c61673c91073d95de7de8893824d4e6612d1b
Ranu <O@O.net>
2017-02-22 12:25:54 +0000

    Angular's CSS Encapsulation

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,8 +3,12 @@ import { Component, Input } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [`
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 
-export class EventThumbnailComponent { 
-    @Input() event:any
-  }
+export class EventThumbnailComponent {
+    @Input() event: any
+}
