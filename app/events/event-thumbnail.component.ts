a0a8e4ca90fe03c92ed6e0204ff5961c7fd83ea8
Ranu <O@O.net>
2017-02-22 17:12:00 +0000

    How to Styling Components with ngClass

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,7 +3,11 @@ import { Component, Input } from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
+    // !important; --> is need otherwise the style will get overridden
     styles: [`
+    .blue { color: #0000FF !important; }
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -12,4 +16,15 @@ import { Component, Input } from '@angular/core'
 
 export class EventThumbnailComponent {
     @Input() event: any
+
+    getStartTimeClassStyleUsingObject() {
+        const isOnline = this.event && this.event.onlineUrl
+        return { blue: isOnline, bold: isOnline }
+    }
+
+    getStartTimeClassStyleUsingArray() {
+        if (this.event && this.event.location)
+            return ['green', 'bold'] // class name in the style
+        return []
+    }
 }
