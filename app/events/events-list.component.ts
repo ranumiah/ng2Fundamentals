5c73f6fa5b2f96710a856ecd938fc472158e5efd
Ranu <ranu.miah@outlook.com>
2017-02-22 07:27:48 +0000

    Add TemplateUrl, which is relative to the location of index.html

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'events-list',
-    template: `
-    <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
-  `,
+    templateUrl: 'app/events/events-list.component.html',
 })
 
 export class EventListComponent {
