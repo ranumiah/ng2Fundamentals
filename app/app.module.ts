f48ed1a3e573e5d3835d4f0b4a8c645f8e0d398c
Ranu <O@O.net>
2017-02-28 17:47:19 +0000

    How to Organizing Your Exports with Barrels (index.ts files)

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
 
+
+import {
+    EventListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
+
 import { EventsAppCoponent } from './events-app.component'
-import { EventListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventListResolver } from './events/events-list-resolver.service'
 
 // So this guy describes the class that follows it
 @NgModule({
