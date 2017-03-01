db6644409e9a279f95a628abe61fafb8056a29c6
Ranu <O@O.net>
2017-03-01 15:41:10 +0000

    How to Filter Data Example

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
 
 @Component({
@@ -8,6 +8,24 @@ import { ISession } from '../shared/index';
 })
 export class SessionListComponent {
     @Input() sessions: ISession[];
+    @Input() filterBy: string;
+    visibleSessions: ISession[] = [];
+
+    // This will get call everything one of the @Input() value is changes    
+    ngOnChanges() {
+        if (this.sessions) {
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter) {
+        if (filter === 'all') {
+            // This crate a complete duplicate array with all the element
+            this.visibleSessions = this.sessions.slice(0);
+        } else {
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
 }
-
-
