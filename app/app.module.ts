119dc239fc997010387f5a54caf60cbea166b89a
Ranu <O@O.net>
2017-03-01 17:19:18 +0000

    Adding JQuery and Organising Common Services with Barrel

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-// Third Party Components
-import { TOASTR_TOKEN, IToastr } from './common/toastr.service'
-
 // My Components
 import {
     EventListComponent,
@@ -21,14 +18,21 @@ import {
     DurationPipe
 } from './events/index'
 
+import {
+    JQ_TOKEN,
+    TOASTR_TOKEN,
+    IToastr,
+    CollapsibleWellComponent
+} from './common/index'
+
 import { EventsAppCoponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { CollapsibleWellComponent } from './common/collapsible-well.component'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
 
 declare let toastr: IToastr
+declare let jQuery: Object
 
 // So this guy describes the class that follows it
 @NgModule({
