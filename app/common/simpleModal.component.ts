e28743f93f140556cab67d07e10b16b972831f45
Ranu <ranu.miah@outlook.com>
2017-03-04 14:56:04 +0000

    Using the @ViewChild Decorator to Access a Specific DOM node

--- app/common/simpleModal.component.ts
+++ app/common/simpleModal.component.ts
@@ -4,14 +4,14 @@ import { JQ_TOKEN } from './jQuery.service'
 @Component({
   selector: 'simple-modal',
   template: `
-  <div id="{{elementId}}" class="modal fade" tabindex="-1">
+  <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
           <h4 class="modal-title">{{title}}</h4>
         </div>
-        <div class="modal-body">
+        <div class="modal-body" (click)="closeModal()">
           <ng-content></ng-content>
         </div>
       </div>
@@ -25,4 +25,15 @@ import { JQ_TOKEN } from './jQuery.service'
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalcontainer') containerEl: ElementRef;
+  @Input() closeOnBodyClick: string;
+
+
+  constructor( @Inject(JQ_TOKEN) private $: any) { }
+
+  closeModal() {
+    if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
+  }
 }
