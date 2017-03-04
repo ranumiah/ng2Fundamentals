6ac03f5198032f22ae341845f4fa02c9934ea12f
Ranu <ranu.miah@outlook.com>
2017-03-04 16:36:49 +0000

    Adding a Validator to Angular's Validators

--- app/events/location-validator.directive.ts
+++ app/events/location-validator.directive.ts
@@ -1,8 +1,13 @@
 import { Directive } from '@angular/core';
-import { Validator, FormGroup } from '@angular/forms';
+// NG_VALIDATORS ==> This supports every validator in Angular 2
+import { Validator, FormGroup, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
-    selector: '[validateLocation]'
+    selector: '[validateLocation]',
+    // We can register this Validator to the list of Angular 2 Validators
+    // multi: true ==> With this we are telling it NOT TO REPLACE existing NG2 Validators
+    // But instead via setting it true we are saying just add this to the existing NG_VALIDATORS collection
+    providers: [{ provide: NG_VALIDATORS, useExisting: LocationValidator, multi: true }]
 })
 export class LocationValidator implements Validator {
     validate(formGroup: FormGroup): { [key: string]: any } {
