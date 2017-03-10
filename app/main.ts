e3118561167e06b5a8d6b99c4a48ba60e185b9de
Ranu <O@O.net>
2017-03-10 17:27:49 +0000

    Enabling Production Mode

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+// This will force it to run in Prod Mode
+// import { enableProdMode } from '@angular/core';
+// enableProdMode();
+
 // This is using (JIT) compilation in the browser with platformBrowserDynamic to bootstrap the AppModule.
 // One time Bootstrap and load the initial main container
 platformBrowserDynamic().bootstrapModule(AppModule);
